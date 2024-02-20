d209684ac5b240038ecb2d2c74ef871492af5e34
Kilian Boute <kilian.boute@gmail.com>
2024-02-20 15:48:25 +0100

    Add simple to do list to display post it notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
 // App.js
-import React from "react";
+import React, { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import PostIt from "./PostIt";
-import DroppableArea from "./DroppableArea";
 
 function App() {
-  const postits = new Array(5).fill(null);
-  const listPostits = postits.map((postit, index) => {
-    return <PostIt key={index} />;
-  });
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          text: newItem,
+        },
+      ];
+    });
+    setNewItem("");
+  };
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <DroppableArea>
-        <div className="App">
-          <h1>TO DO</h1>
-          {listPostits}
-        </div>
-      </DroppableArea>
-    </DndProvider>
+    <div>
+      <h2>To do list</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="item">Enter Text:</label>
+
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+          name="item"
+        />
+
+        <input type="submit" value="Submit" />
+      </form>
+
+      <div className="todoList">
+        {todos.map((todo) => {
+          return (
+            <div className="post-it" key={todo.id}>
+              {todo.text}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
